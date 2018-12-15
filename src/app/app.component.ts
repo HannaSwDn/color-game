a95255be4deac1fee79ec2c8b7a71e9a5cb09a75
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-15 13:34:55 +0100

    Removes content from home.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
@@ -12,9 +11,7 @@ export class MyApp {
   rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
