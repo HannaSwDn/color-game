b8653e58049789cda7448174f6c4dc223de4409f
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-15 13:52:52 +0100

    Adds spec files for home.html, app and app component

---
+++ src/pages/home/home.spec.ts
@@ -0,0 +1,47 @@
+import { HomePage } from "./home";
+import { TestBed, async } from "@angular/core/testing";
+import { IonicModule, Platform, NavController } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock } from "ionic-mocks";
+
+describe("HomePage", () => {
+  let homepage;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        HomePage
+      ],
+      imports: [IonicModule.forRoot(HomePage)],
+      providers: [
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
+        { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
+        { provide: NavController, useFactory: () => NavControllerMock.instance() }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePage);
+    homepage = fixture.componentInstance;
+  });
+
+	it("should create the home page", () => {
+    expect(homepage).toBeTruthy();
+    expect(homepage instanceof HomePage).toEqual(true);
+  });
+
+  it('should have user default values', () => {
+    expect(homepage.user).toEqual({ distance: 1000, age: 20 });
+  });
+
+  it('should have calculate function', () => {
+    spyOn(homepage, 'calculate'); // we use jasmine to spy on a function
+
+    homepage.calculate()
+
+    expect(homepage.calculate).toHaveBeenCalled(); // check if the function has been called
+  });
+});
