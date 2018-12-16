706b62dec4465fb45a7e103cd917f301b9ddff88
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-16 10:54:20 +0100

    Adds DOM variables, adds array containing colors

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -13,7 +13,14 @@ export class GamePage {
   }
 
   startGame() {
-    
-  }
+    // DOM variables
+    let paragraph: HTMLElement = document.getElementById('paragraph');
+    let wrapper: HTMLElement = document.querySelector('.wrapper');
+    // variables
+    let colors_array: string[] = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown'];
+    let color_text_array: string[] = colors_array;
 
+    paragraph.innerHTML = colors_array[Math.floor(Math.random() * colors_array.length + 1)];
+    wrapper.style.backgroundColor = colors_array[Math.floor(Math.random() * colors_array.length + 1)];
+  }
 }
