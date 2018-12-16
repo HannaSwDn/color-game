951f11432a37b2e3e8f3da999d3f694db4ad488d
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-16 10:29:38 +0100

    Removes non-needed pages, cleans up game page and home page

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -4,8 +4,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-game',
-  templateUrl: 'game.html',
+  templateUrl: 'game.html'
 })
+
 export class GamePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
